f0136d2786e5d622ea96404a445121834672a4fa
gurman <gurmansandhusaab@gmail.com>
2019-06-13 11:44:30 +0800

    pull

--- capstone/frontend/src/index.js
+++ capstone/frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import Summary from './components/Summary'
+import Entrepreneurs from './components/Entrepreneurs';
+import Sidebar from './components/sidebar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Entrepreneurs />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
